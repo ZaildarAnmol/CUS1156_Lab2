5b2553eb3dc09462f8ceceb1807259edc6bb895c
Anmol Singh <anmolzaildar4@gmail.com>
2023-10-02 01:28:52 -0400

    Lab2 updated

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,61 @@
+/* Lab: 2
+* Class: CUS1156
+* Author: ANMOL SINGH
+* Date: 09/02/2023
+* THIS class counts the number of distinct elements in an ArrayList<String>.
+*/
+
+
 import java.util.ArrayList;
 
 public class UniqueWords
 {
    /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
+    * Counts the number of unique strings in a list.
+    * @param list ArrayList of strings to be examined.
+    * @return Number of unique strings in the list.
+    */
+	
+	// this countUnique method manually checks for duplicates by comparing each element with all previous elements. 
    public static int countUnique(ArrayList<String> list)
    {
-	  int count = 0;
-	  
+      int count = 0;
+
       for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
+      {
+         boolean UniqueNumber = true;
+         for (int j = 0; j < i; j++)
+         {
+            if (list.get(i).equals(list.get(j)))
+            {
+               UniqueNumber = false;
+               break; 
+            }
+         }
+
+         if (UniqueNumber)
+         {
+            count++;
+         }
       }
-	  return count;
+
+      return count;
    }
+   
+// ----------------------------------------------------------------
 
    public static void main(String[] args)
    {
-      ArrayList <String> words = new ArrayList<>();
+      ArrayList<String> words = new ArrayList<>();
       words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
+      words.add("orange");
+      words.add("blackboard");
+      words.add("apple");
+      words.add("orange");
+      words.add("sun");
+      words.add("moon");
+
+      int unique = countUnique(words);
       System.out.println(words + " has " + unique + " unique words");
    }
 }
